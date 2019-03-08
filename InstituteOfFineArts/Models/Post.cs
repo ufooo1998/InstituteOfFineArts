662f001d4db6823b99df16e1370ffd3f04b8b242
UFO <yurnero1681998@gmail.com>
2019-03-08 11:59:24 +0700

    UpgradeHome

--- InstituteOfFineArts/Models/Post.cs
+++ InstituteOfFineArts/Models/Post.cs
@@ -1,6 +1,7 @@
 ﻿using InstituteOfFineArts.Areas.Identity.Data;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,12 +10,30 @@ namespace InstituteOfFineArts.Models
     public class Post
     {
         public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "Post Name")]
         public string PostName { get; set; }
+
+        [Display(Name = "Image")]
         public byte[] Image { get; set; }
+
+        [Required]
+        [Display(Name = "Decription")]
         public string Decription { get; set; }
+
+        [Display(Name = "Start Date")]
         public int Price { get; set; }
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dddd - dd/MM/yyyy - HH:mm}")]
+        [Display(Name = "Create Date")]
         public DateTime CreatedAt { get; set; }
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dddd - dd/MM/yyyy - HH:mm}")]
+        [Display(Name = "Last Update")]
         public DateTime UpdatedAt { get; set; }
+
+        [Display(Name = "Author")]
         public string UserID { get; set; }
         public CustomUser User { get; set; }
         public List<CompetitionPost> CompetitionPosts { get; set; }
